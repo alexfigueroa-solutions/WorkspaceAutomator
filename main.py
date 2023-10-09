d8f5db5f929010438485712ba031697050635fd4
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-10-09 10:45:51 -0700

    writes initial code for main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+import json
+from utils.window_manager import WindowManager
+from utils.process_launcher import ProcessLauncher
+from config import DEFAULT_PROFILE_PATH
+
+class WorkspaceAutomator:
+    def __init__(self, profile_path=DEFAULT_PROFILE_PATH):
+        self.profile = self._load_profile(profile_path)
+        self.process_launcher = ProcessLauncher()
+        self.window_manager = WindowManager()
+
+    def _load_profile(self, profile_path):
+        with open(profile_path, 'r') as f:
+            return json.load(f)
+
+    def launch_applications(self):
+        for app in self.profile.get("applications", []):
+            self.process_launcher.launch(app)
+            self.window_manager.position_window(app)
+
+    def run(self):
+        self.launch_applications()
+
+def main(profile_path):
+    automator = WorkspaceAutomator(profile_path)
+    automator.run()
+
+if __name__ == "__main__":
+    # This allows for the optional passing of a custom profile path
+    profile_path = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_PROFILE_PATH
+    main(profile_path)
